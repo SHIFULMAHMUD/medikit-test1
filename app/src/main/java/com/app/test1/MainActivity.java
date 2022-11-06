46933b2b79340702d0b2f5884cce7ef1c35d20e0
SHIFUL MAHMUD <shifulmahmudkhan@gmail.com>
2022-11-06 17:09:22 +0600

    update

--- app/src/main/java/com/app/test1/MainActivity.java
+++ app/src/main/java/com/app/test1/MainActivity.java
@@ -48,12 +48,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textViewPulse,textViewOxygen,textViewTemperature,textViewHeight;
+    TextView textViewHeight;
     private ApiInterface apiInterface;
-    ProgressBar progressBar;
     ConstraintLayout layout;
-    SwipeRefreshLayout mSwipeRefreshLayout;
-    double pulse,oxygen,temperature;
+    SwipeRefreshLayout refreshLayout;
     MediaPlayer mediaPlayer;
     boolean prepared=false;
     boolean started=false;
@@ -79,16 +77,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewPulse = findViewById(R.id.textViewPulse);
-        textViewOxygen = findViewById(R.id.textViewOxygen);
         textViewHeight = findViewById(R.id.textViewHeight);
-        textViewTemperature = findViewById(R.id.textViewTemperature);
-        progressBar= findViewById(R.id.progressBar);
         buttonPlay=findViewById(R.id.buttonPlay);
-        mSwipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        refreshLayout = findViewById(R.id.swipeRefreshLayout);
         layout = findViewById(R.id.layout);
-
-        // initialize all variables with their layout items.
         statusTV = findViewById(R.id.idTVstatus);
         startTV = findViewById(R.id.btnRecord);
         stopTV = findViewById(R.id.btnStop);
@@ -101,44 +93,34 @@ public class MainActivity extends AppCompatActivity {
         startTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // start recording method will
-                // start the recording of audio.
                 startRecording();
             }
         });
         stopTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // pause Recording method will
-                // pause the recording of audio.
                 pauseRecording();
-
             }
         });
         playTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // play audio method will play
-                // the audio which we have recorded
                 playAudio();
             }
         });
         stopplayTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // pause play method will
-                // pause the play of audio
                 pausePlaying();
             }
         });
 
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.purple_700);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        refreshLayout.setColorSchemeResources(R.color.purple_700);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                getData();
+                refreshLayout.setRefreshing(true);
                 getHeightData();
-                mSwipeRefreshLayout.setRefreshing(false);
             }
         });
         buttonPlay.setEnabled(false);
@@ -160,150 +142,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        getData();
         getHeightData();
     }
 
-    private void getData() {
-        layout.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
-        apiInterface = ApiClient.getClient().create(ApiInterface.class);
-        Call<Data> call = apiInterface.getData("CRJVGC27S9JP8HHI");
-        call.enqueue(new Callback<Data>() {
-            @Override
-            public void onResponse(Call<Data> call, Response<Data> response) {
-                if (response.code()==200) {
-                    progressBar.setVisibility(View.GONE);
-                    layout.setVisibility(View.VISIBLE);
-                    Data body=response.body();
-                    textViewPulse.setText(body.getFeeds().get(body.getFeeds().size()-1).getField1());
-                    textViewOxygen.setText(body.getFeeds().get(body.getFeeds().size()-1).getField2());
-                    textViewTemperature.setText(body.getFeeds().get(body.getFeeds().size()-1).getField3());
-                    pulse=Double.parseDouble(body.getFeeds().get(body.getFeeds().size()-1).getField1());
-                    oxygen=Double.parseDouble(body.getFeeds().get(body.getFeeds().size()-1).getField2());
-                    temperature=Double.parseDouble(body.getFeeds().get(body.getFeeds().size()-1).getField3());
-
-                    if (pulse<60 && oxygen<94 && temperature<97) {
-                        sendNotification("Your Pulse rate, Oxygen level & Temperature is Low!");
-                    }
-                     if(pulse>100 && oxygen>100 && temperature>99)
-                    {
-                        sendNotification("Your Pulse rate, Oxygen level & Temperature is High!");
-                    }
-                     if(pulse>100 && oxygen>100)
-                    {
-                        sendNotification("Your Pulse rate & Oxygen level is High!");
-                    }
-                     if(pulse>100 && temperature>99)
-                    {
-                        sendNotification("Your Pulse rate & Temperature is High!");
-                    }
-                     if(oxygen>100 && temperature>99)
-                    {
-                        sendNotification("Your Oxygen level & Temperature is High!");
-                    }
-                     if(pulse<60 && oxygen<94)
-                    {
-                        sendNotification("Your Pulse rate & Oxygen level is Low!");
-                    }
-                     if(pulse<60 && temperature<97)
-                    {
-                        sendNotification("Your Pulse rate & Temperature is Low!");
-                    }
-                     if(oxygen<94 && temperature<97)
-                    {
-                        sendNotification("Your Oxygen level & Temperature is Low!");
-                    }
-                     if(pulse>100)
-                    {
-                        sendNotification("Your Pulse rate is High!");
-                    }
-                     if(oxygen>100)
-                    {
-                        sendNotification("Your Oxygen level is High!");
-                    }
-                     if(temperature>99)
-                    {
-                        sendNotification("Your Temperature is High!");
-                    }
-                     if(pulse<60)
-                    {
-                        sendNotification("Your Pulse rate is Low!");
-                    }
-                     if(oxygen<94)
-                    {
-                        sendNotification("Your Oxygen level is Low!");
-                    }
-                     if(temperature<97)
-                    {
-                        sendNotification("Your Temperature is Low!");
-                    }
-                }
-            }
-            @Override
-            public void onFailure(Call<Data> call, Throwable t) {
-                progressBar.setVisibility(View.GONE);
-                layout.setVisibility(View.VISIBLE);
-                t.printStackTrace();
-            }
-        });
-    }
-
     private void getHeightData() {
-        layout.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
+        refreshLayout.setRefreshing(false);
         apiInterface = ApiClient.getClient().create(ApiInterface.class);
         Call<Height> call = apiInterface.getHeightData("GEAWZO7Y24UC8F58");
         call.enqueue(new Callback<Height>() {
             @Override
             public void onResponse(Call<Height> call, Response<Height> response) {
                 if (response.code()==200) {
-                    progressBar.setVisibility(View.GONE);
-                    layout.setVisibility(View.VISIBLE);
                     Height body=response.body();
                     textViewHeight.setText(body.getFeeds().get(body.getFeeds().size()-1).getField1());
+                    getHeightData();
                 }
             }
             @Override
             public void onFailure(Call<Height> call, Throwable t) {
-                progressBar.setVisibility(View.GONE);
-                layout.setVisibility(View.VISIBLE);
                 t.printStackTrace();
+                Log.d("fail",t.getLocalizedMessage());
             }
         });
-    }
-
-    private void sendNotification(String details) {
-        Uri urinotification = null;
-        try {
-            urinotification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), urinotification);
-            r.play();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel("Notification", "Notification", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(notificationChannel);
-        }
-
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this, "Notification")
-                        .setSmallIcon(R.drawable.logo)
-                        .setContentTitle(getString(R.string.alert))
-                        .setSound(urinotification)
-                        .setContentText(details);
-
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-
-// Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
     }
 
     class PlayerTask extends AsyncTask<String,Void,Boolean>{
@@ -345,13 +205,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startRecording() {
-        // check permission method is used to check
-        // that the user has granted permission
-        // to record nd store the audio.
         if (CheckPermissions()) {
-
-            // setbackgroundcolor method will change
-            // the background color of text view.
             stopTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
             startTV.setBackgroundColor(getResources().getColor(R.color.gray));
             playTV.setBackgroundColor(getResources().getColor(R.color.gray));
@@ -433,7 +287,6 @@ public class MainActivity extends AppCompatActivity {
         // permission for audio recording and storage.
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
     }
-
 
     public void playAudio() {
         stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
